8f70e4163b09773905542832460e7971e703d1dd
Tetrismember <dn852787@gmail.com>
2025-01-09 16:08:49 +0700

    Add files via upload

---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+namespace Tetris
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "server=localhost;Database=loginDB;User ID= root; Password=19283746";
+            using (MySqlConnection sqlCon = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    if (sqlCon.State == ConnectionState.Closed)
+                        sqlCon.Open();
+                    string query = "SELECT COUNT(1) FROM tbluser WHERE Username = @Username AND Password = @Password";
+                    MySqlCommand sqlCmd = new MySqlCommand(query, sqlCon);
+                    sqlCmd.CommandType = CommandType.Text;
+                    sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Connection Error");
+
+
+                }
+            }
+        }
+    }
+}
